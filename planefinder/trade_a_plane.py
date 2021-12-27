2a9f0cf80c12204f2b34297ca16d790890c5f573
Jason Walleser <jwalleser@gmail.com>
2021-12-27 09:05:10 -0700

    Implemented more `trade_a_plane` functions.
    Functions implemented: make_model, price, registration, description,
    and ttaf

--- planefinder/trade_a_plane.py
+++ planefinder/trade_a_plane.py
@@ -7,6 +7,7 @@ first matching data.
 
 import re
 from datetime import datetime
+from typing import List
 from bs4 import Tag
 
 def is_listing_result(tag: Tag):
@@ -47,6 +48,7 @@ def last_update(node: Tag) -> datetime:
     update_node = node.find(name='p', class_='last-update')
     pattern = r'\d{2}/\d{2}/\d{4}'
     search_result = re.search(pattern, update_node.text).group(0)
+    date_fmt = '%m/%d/%Y'
     date_ = datetime.strptime(search_result, date_fmt)
     return date_
 def make_model(node: Tag):
@@ -56,12 +58,22 @@ def make_model(node: Tag):
     Parameters
     ----------
     node: Tag
-        A detail page
-        <div id='main_info'>
+        A detail page expected to contain a <li class="makeModel> node
+    Returns
+    -------
+    processed_model: str
+        Make/Model text or 'Unknown' if not found. Example: Cessna 182 Q Skylane.
     """
-    
+    # Get model node, <li class="makeModel">
+    model_node = node.find(name='li', class_='makeModel')
+    model_text = model_node.text
+    # Capture model text, everything after the last colon, stripped of spaces
+    processed_model = _text_after_colon_and_strip(model_text)
+    if processed_model == '':
+        processed_model = 'Unknown'
+    return processed_model
 
-def price(node):
+def price(node: Tag) -> float:
     """
     Get price from a node.
     """
@@ -73,22 +85,95 @@ def price(node):
         raise ValueError('Expected to find a <span itemprop="price"> tag')
     return float(price_text)
 
-def registration(node):
+def registration(node: Tag) -> str:
     """
     Get registration (N-number) from a node.
-    """
 
-def description(node):
+    Parameters
+    ----------
+    node: Tag
+        Tag from detail page. Expect info in <li class="reg-number">
+    """
+    reg_txt = node.find(name='li', class_='reg-number').text
+    reg =_text_after_colon_and_strip(reg_txt)
+    if reg == '':
+        reg = 'Unknown'
+    return reg
+
+def description(node: Tag) -> str:
     """
     Get seller's description from a node.
-    """
 
-def ttaf(node):
+    Parameters
+    ----------
+    node: Tag
+        Detail page containing an element with id="detailed_desc" which
+        contains another tag with itemprop="description"
+    
+    Returns
+    -------
+    text: str
+        Text as provided by seller
+    """
+    return node.find(id='detailed_desc').find(itemprop='description').text
+
+def ttaf(node: Tag):
     """
     Get total time airframe from a node.
     """
+    spec_list = _general_specs(node)
+    for spec in spec_list:
+        tokens = spec.split(':')
+        if tokens[0].strip().lower() == 'total time':
+            return float(tokens[1])
+    return -1
 
-def smoh(node):
+def smoh(node: Tag):
     """
     Get time since major overhaul from a node.
+    """
+    spec_list = _general_specs(node)
+def _text_after_colon_and_strip(text: str):
+    pattern = r':(.+$)'
+    result = re.search(pattern, text)
+    if result is None:
+        return ''
+    return result.group(1).strip()
+
+def _general_specs(node: Tag) -> List[str]:
+    """
+    Parameters
+    ----------
+    node: Tag
+        Detail page containing <div id='general_specs'>
+    
+    Returns
+    -------
+    general_specs: List[Tag]
+        A list of general spec field text
+        Example: [Total Time:3388,
+                  Engine 1 Time:271 SMOH,
+                  Prop 1 Time:410 SMOH,
+                  Useful Load:1100,
+                  Condition:Used,
+                  Year Painted:1994,
+                  Interior Year:1992,
+                  Flight Rules:IFR,
+                  # of Seats:4]
+    """
+    general_specs = node.find(id='general_specs')
+    if general_specs is None:
+        return []
+    spec_paragraphs = general_specs.find_all(name='p')
+    if spec_paragraphs is None:
+        return []
+    return [general_spec_node.text for general_spec_node in spec_paragraphs]
+
+def _spec_from_colon_separated_text_list(spec_name: str):
+    """
+    Get a specification value from a list of colon separated values in
+
+    `_general_specs(...)` returns a list of strings of with the format
+    `Spec Name: Spec Value`.
+    TODO: Implement and use this. Refactor `ttaf(...)`
     """
