c121b1bab14a596a739013e60ee9cb128b107a58
Jason Walleser <jwalleser@gmail.com>
2022-04-22 11:21:36 -0600

    Add app level logging

---
+++ tests/test_logging.py
@@ -0,0 +1,8 @@
+from planefinder import logging
+from pathlib import Path
+
+def test_logging_setup():
+    log_file = Path(__file__).parent.joinpath("logging-test.log")
+    logging.setup_applevel_logger(file_name=log_file)
+    log = logging.get_logger(__file__)
+    log.debug("Logging debug")
