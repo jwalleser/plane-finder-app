a73725e36eae80af88deaf797a7648ae13375f14
Jason Walleser <jwalleser@gmail.com>
2022-04-24 08:49:34 -0600

    More listing navigation features. - Moved reading HTML to planefinder.utils - Added navigation from listing entry to listing detail

--- planefinder/crawler-mobile-1.py
+++ planefinder/crawler-mobile-1.py
@@ -1,81 +1,53 @@
 from bs4 import BeautifulSoup
 from planefinder import trade_a_plane
-from planefinder import utils
 
 class ListingsPage:
-    def __init__(self, listing_page_url: str):
-        self.url = listing_page_url
-        self.page_soup = utils.read_html_into_soup(listing_page_url)
-
+    def __init__(self, page_soup: BeautifulSoup):
+        self.page_soup = page_soup
+    
     @property
     def entries(self):
         for entry_soup in self.page_soup.find_all(trade_a_plane.is_listing_result):
-            yield ListingEntry(self, entry_soup)
+            yield ListingEntry(entry_soup)
 
 
 class ListingEntry:
-    def __init__(self, listings_page: ListingsPage, listing_soup: BeautifulSoup):
-        self.listings_page = listings_page
+    def __init__(self, listing_soup: BeautifulSoup):
         self.listing_soup = listing_soup
-
-    @property
-    def id(self):
-        return trade_a_plane.listing_id(self.listing_soup)
-
-    @property
-    def seller(self):
-        return trade_a_plane.seller_id(self.listing_soup)
-
-    @property
-    def last_update(self):
-        return trade_a_plane.last_update(self.listing_soup)
 
     @property
     def listing_detail(self):
         return ListingDetail()
-
-    @property
-    def detail_url(self):
-        return trade_a_plane.detail_page_url(self.listing_soup)
 
 
 class ListingDetail:
     @property
     def listing_id(self):
         pass
-
     @property
     def seller_id(self):
         pass
-
     @property
     def last_update(self):
         pass
-
     @property
     def make_model(self):
         pass
-
     @property
     def price(self):
         pass
-
     @property
     def registration(self):
         pass
-
     @property
     def description(self):
         pass
-
     @property
     def ttaf(self):
         pass
-
     @property
     def engine_time(self):
         pass
-
     @property
     def smoh(self):
         pass
---
+++ planefinder/utils.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def read_html_into_soup(html_file):
+    if is_http(html_file):
+        html = get_webpage(html_file)
+    else:
+        with open(html_file) as f:
+            html = f.read()
+    soup = BeautifulSoup(html, features="html.parser")
+    return soup
+
+
+def get_webpage(address):
+    response = requests.get(address)
+    return response.text
+
+
+def is_http(path: str) -> bool:
+    return str(path).lower().startswith("http")
