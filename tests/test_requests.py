083d82891e3e7579d8bf8ab5409c98f1889a5d1c
Jason Walleser <jwalleser@gmail.com>
2024-12-14 07:05:46 -0700

    Find "trade-a-plane" not <!doctype html>

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -5,4 +5,4 @@ def test_request_get_trade_a_plane_home():
     response = requests.get("https://www.trade-a-plane.com")
     html = response.text
     assert len(html) > 0
-    assert html.lower().startswith("<!doctype html>")
+    assert html.lower().find("trade-a-plane") > 0
